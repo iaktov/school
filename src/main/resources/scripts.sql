668c8dc8edbfdac90810ebf2607f6849caf55708
aktov <aktovi@gmail.com>
2022-09-03 09:40:14 +0700

    Homework_3.3(with_test)

---
+++ src/main/resources/scripts.sql
@@ -0,0 +1,24 @@
+select *
+from student
+where age > 23
+  and age < 26;
+
+select student.name
+from student;
+
+select *
+from student
+where name like '%o%';
+
+select *
+from student
+where age < student.id;
+
+select *
+from student
+order by age;
+
+
+
+
+
